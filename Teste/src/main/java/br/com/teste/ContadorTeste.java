d234cf69909ac011348b9fea783ef9cac0529600
Danifleikson Buriti Lisboa <danifleikson@hotmail.com>
2022-11-09 21:59:51 -0300

    Criento o contador

---
+++ Teste/src/main/java/br/com/teste/ContadorTeste.java
@@ -0,0 +1,22 @@
+package br.com.teste;
+
+public class ContadorTeste {
+
+	public static void main(String[] args) {
+
+				for (int n = 1; n <= (int) 100; n++) {
+
+					if (n % 3 == 0) {
+						System.out.println("foo");
+					}
+					if (n % 5 == 0) {
+						System.out.println("baa");
+					}
+					System.out.println(n);
+				}
+
+			}
+
+		}
+
+
---
+++ Teste/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module Teste {
+	exports br.com.teste;
+
+	requires spring.beans;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.context;
+	requires spring.core;
+}
